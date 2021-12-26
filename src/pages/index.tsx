550b2c5ef0e3ec2736e0cf74b2e1cbe828a741c8
Shoji Makino <pitang1965@gmail.com>
2021-12-26 12:30:45 +0900

    Add src folder and move pages folder to it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React, { useCallback, useEffect, useState, Fragment } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 import Airtable from 'airtable';
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
