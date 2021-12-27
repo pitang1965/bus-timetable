568efefb19cdfecbdc36413816c2f3475ea1c65d
Shoji Makino <pitang1965@gmail.com>
2021-12-27 23:57:13 +0900

    Fix: ListBox z-index problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useEffect, useState, Fragment } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
 import Airtable from 'airtable';
-import { Listbox, Transition } from '@headlessui/react';
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import { StationListBox } from '../components/StationListBox';
 
 const Home: NextPage = () => {
   type Fieldset = {
     id: string;
     name: string;
-    // unavailable: boolean;
   };
   const [stations, setStations] = useState<Fieldset[]>([]);
   const [selectedStationFrom, setSelectedStationFrom] = useState<
+    Fieldset | undefined
+  >();
+  const [selectedStationTo, setSelectedStationTo] = useState<
     Fieldset | undefined
   >();
   const formattedRecords: Fieldset[] = [];
@@ -61,68 +62,19 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.svg' />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-        <Listbox value={selectedStationFrom} onChange={setSelectedStationFrom}>
-          <div className='relative mt-4'>
-            <Listbox.Label>出発地: </Listbox.Label>
-            <Listbox.Button className='relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm'>
-              <span className='block truncate'>
-                {selectedStationFrom?.name}
-              </span>
-              <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
-                <SelectorIcon
-                  className='w-5 h-5 text-gray-400'
-                  aria-hidden='true'
-                />
-              </span>
-            </Listbox.Button>
-            <Transition
-              as={Fragment}
-              leave='transition ease-in duration-100'
-              leaveFrom='opacity-100'
-              leaveTo='opacity-0'
-            >
-              <Listbox.Options className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
-                {stations.map((station) => (
-                  <Listbox.Option
-                    key={station.id}
-                    className={({ active }) =>
-                      `${
-                        active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'
-                      }
-                          cursor-default select-none relative py-2 pl-10 pr-4`
-                    }
-                    value={station}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`${
-                            selected ? 'font-medium' : 'font-normal'
-                          } block truncate`}
-                        >
-                          {station.name}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={`${
-                              active ? 'text-amber-600' : 'text-amber-600'
-                            }
-                                absolute inset-y-0 left-0 flex items-center pl-3`}
-                          >
-                            <CheckIcon className='w-5 h-5' aria-hidden='true' />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </Listbox>
+        <h1 className={styles.title}>バス時刻表</h1>
+        <StationListBox
+          label='出発地：'
+          stations={stations}
+          selected={selectedStationFrom}
+          setSelected={setSelectedStationFrom}
+        />
+        <StationListBox
+          label='行き先：'
+          stations={stations}
+          selected={selectedStationTo}
+          setSelected={setSelectedStationTo}
+        />
       </main>
       <footer className={styles.footer}>
         <a
