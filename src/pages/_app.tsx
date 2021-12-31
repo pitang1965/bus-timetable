d7c9c5e4b1793c26b12cae9c61769e1e23ac3ca5
Shoji Makino <pitang1965@gmail.com>
2021-12-31 13:45:06 +0900

    Add Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto my-10 max-w-xl bg-blue-100">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
