acbb2fbb8ca7fa9614b9546e1f0007dc00d89806
Shoji Makino <pitang1965@gmail.com>
2022-01-02 21:09:37 +0900

    Sort time table data

--- src/components/TimeTable.tsx
+++ src/components/TimeTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { VFC } from 'react';
-import type { FieldSet } from 'airtable';
+import type { FieldSet, Record } from 'airtable';
 
 type TimeTableProps = {
   from: FieldSet | undefined;
@@ -8,8 +8,27 @@ type TimeTableProps = {
   data: FieldSet[] | undefined;
 };
 
+const TableBody = (props) => {
+  console.log(props.data);
+  return (
+    <main>
+      <ul>
+        {props.data.map((record) => (
+          <li key={record.id}><p>{`#${record.fields.Id} ${record.fields.From} --> ${record.fields.To} ${record.fields.Hour}:${record.fields.Minute} `}</p></li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
 export const TimeTable: VFC<TimeTableProps> = (props) => {
-  return <div className='mt-4'>
-    {props.from === props.to ? '出発地と行き先が同じです':'タイムテーブル'}
-  </div>;
+  return (
+    <div className='mt-4'>
+      {props.from === props.to ? (
+        '出発地と行き先が同じです'
+      ) : (
+        <TableBody data={props.data} />
+      )}
+    </div>
+  );
 };
