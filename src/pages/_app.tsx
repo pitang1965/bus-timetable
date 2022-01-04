51a84d69d85409e330ebb6d4654cbe52457e7fb4
Shoji Makino <pitang1965@gmail.com>
2022-01-04 13:46:02 +0900

    Remove vertical margin of container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto my-10 max-w-xl bg-blue-100">
+    <div className="container mx-auto max-w-xl bg-blue-100">
       <Component {...pageProps} />
     </div>
   );
