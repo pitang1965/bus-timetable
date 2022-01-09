cd7679bce4a850e7a2c59c518f1fc0c914d3de1d
Shoji Makino <pitang1965@gmail.com>
2022-01-09 12:20:19 +0900

    Change code about Google Anlytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
+import '../../styles/globals.css';
+import { GA_TRACKING_ID, pageview } from '../lib/gtag';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import '../../styles/globals.css';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -12,6 +16,20 @@ type AppPropsWithLayout = AppProps & {
 };
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   const getLayout = Component.getLayout ?? ((page) => page);
   return getLayout(<Component {...pageProps} />);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,47 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
+import { GA_TRACKING_ID } from '../lib/gtag';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
-type Props = {}
+type Props = {};
+
+const gtag = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 class Document extends NextDocument<Props> {
   render() {
     return (
       <Html lang='ja'>
-        <Head />
+        <Head>
+          {GA_TRACKING_ID && process.env.NODE_ENV === 'production' && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-        <meta name="theme-color" content='#fff' />
+        <meta name='theme-color' content='#fff' />
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default Document
+export default Document;
