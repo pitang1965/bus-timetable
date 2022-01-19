9fd3454421c2c23202da845b4ca243799f19c952
Shoji Makino <pitang1965@gmail.com>
2022-01-19 13:38:38 +0900

    Chage message of About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,11 +10,19 @@ const about = () => {
           本アプリについて
         </h1>
         <article className=' pt-6 ml leading-loose'>
-          <h1 className='text-2xl font-bold mb-2'>概要</h1>
-          <p>品証のまきのSです。</p>
+          <h2 className='text-2xl font-bold'>概要</h2>
+          <p>ME品証のまきのSです。</p>
           <p>これは会社のバスの時刻を調べるためのアプリです。</p>
           <p>2021年度の年末年始で新技術の学習を兼ねて作成したものです。</p>
-          <p>データの問題など不具合があったら問い合わせフォームよりお願いします。</p>
+
+          <h2 className='text-2xl font-bold mt-2'>お願い</h2>
+          <p>
+            本WebアプリはPWAなのでWindows, Android,
+            iOSにインストール可能です。ぜひ、インストールしてお使いください（iOSはSafariの[ホーム画面に追加]より）。
+          </p>
+          <p>
+            データの問題など不具合があったら問い合わせフォームよりお願いします。
+          </p>
         </article>
 
         <figure className='m-4'>
