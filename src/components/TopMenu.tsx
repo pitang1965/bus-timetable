205a361db805e192c016d36bc83587fc87db58df
Shoji Makino <pitang1965@gmail.com>
2022-01-19 13:23:39 +0900

    Chage for upgrading Tailwind CSS from v2 to v3

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -21,11 +21,11 @@ export default function TopMenu() {
         <Transition
           as={Fragment}
           enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
+          enterFrom='opacity-0 scale-95'
+          enterTo='opacity-100 scale-100'
+          leave='ease-in duration-75'
+          leaveFrom='opacity-100 scale-100'
+          leaveTo='opacity-0 scale-95'
         >
           <Menu.Items className='absolute left-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
             <div className='px-1 py-1 '>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   mode: "jit",
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media',
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
