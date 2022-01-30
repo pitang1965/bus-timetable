db0a78727e5ff152f6332591e9691fffa39a8d5d
Shoji Makino <pitang1965@gmail.com>
2022-01-30 18:40:26 +0900

    Introduce @tailwindcss/typography and apply to About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
 import Image from 'next/image';
 import siteUrlBarcode from '../../public/site-url.jpg';
 
@@ -9,29 +10,35 @@ const about = () => {
         <h1 className='flex justify-center text-4xl font-bold text-blue-600/80'>
           本アプリについて
         </h1>
-        <article className='pt-6 leading-loose'>
-          <h2 className='text-2xl font-bold'>概要</h2>
+        <article className='prose lg:prose-xl'>
+          <h2>概要</h2>
           <p>ME品証のまきのSです。</p>
           <p>これは会社のバスの時刻を調べるためのアプリです。</p>
           <p>2021年度の年末年始で新技術の学習を兼ねて作成したものです。</p>
+          <h2>お願い</h2>
+          <p>
+            本Webアプリは<abbr title='Progressive Web App'>PWA</abbr>
+            なのでWindows, Android,
+            iOSにインストール可能です。ぜひ、インストールしてお使いください（
+            <strong>iOS</strong>は<strong>Safari</strong>の
+            <strong>[ホーム画面に追加]</strong>より）。
+          </p>
+          <p>
+            データの問題など不具合があったら
+            <Link href='/contact' passHref>
+              <a>お問い合わせ</a>
+            </Link>
+            問い合わせフォームよりお願いします。
+          </p>
 
-          <h2 className='mt-2 text-2xl font-bold'>お願い</h2>
-          <p>
-            本WebアプリはPWAなのでWindows, Android,
-            iOSにインストール可能です。ぜひ、インストールしてお使いください（iOSはSafariの[ホーム画面に追加]より）。
-          </p>
-          <p>
-            データの問題など不具合があったら問い合わせフォームよりお願いします。
-          </p>
+          <figure>
+            <Image
+              src={siteUrlBarcode}
+              alt='https://j-bus-time-table.vercel.app/'
+            />
+            <figcaption>サイトURL</figcaption>
+          </figure>
         </article>
-
-        <figure className='m-4'>
-          <Image
-            src={siteUrlBarcode}
-            alt='https://j-bus-time-table.vercel.app/'
-          />
-          <figcaption className='text-center'>サイトURL</figcaption>
-        </figure>
       </main>
     </Layout>
   );
