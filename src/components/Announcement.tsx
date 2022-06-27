c961eaac0b1bdb82cf25d19df599dc6a9b7557a0
Pitang1965 <47315420+pitang1965@users.noreply.github.com>
2022-06-27 17:26:22 +0900

    Update Announcement.tsx
    Update Announcement.tsx

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -4,15 +4,7 @@ export const Announcement = () => {
   return (
     <div className='py-3 px-4 mb-2 text-white bg-purple-600'>
       <p className='text-sm font-medium text-center'>
-        <a
-          href='https://todays-lunch.vercel.app/'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='underline'
-        >
-          平田のお弁当注文アプリ
-        </a>
-        もぜひご利用ください。
+        6/28（火）の限定ダイヤにご注意ください。武蔵村山9:00は運休。本社9:00、10:00、11:55発のバスは本社止まりません。村山10:45発も本社止まりません。
       </p>
     </div>
   );
