0cedabc3aaa020385732e96a6faa0c96b1ad02fe
Pitang1965 <47315420+pitang1965@users.noreply.github.com>
2022-07-29 13:49:49 +0900

    Changes due to service termination

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -4,15 +4,7 @@ export const Announcement = () => {
   return (
     <div className='py-3 px-4 mb-2 text-white bg-purple-600'>
       <p className='text-sm font-medium text-center'>
-        <a
-          href='https://todays-lunch.vercel.app/'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='underline'
-        >
-          平田のお弁当注文アプリ
-        </a>
-        もぜひご利用ください。2022-07-01の時刻表改定に対応済です。
+        本アプリはサービスを中止しました。
       </p>
     </div>
   );
